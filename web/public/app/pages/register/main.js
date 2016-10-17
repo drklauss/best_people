834b148fe5ec173e5db15311876c7f7fda5d8099
alexey <dr.klauss@gmail.com>
2016-10-18 00:47:16 +0300

    layout register page

--- web/public/app/pages/register/main.js
+++ web/public/app/pages/register/main.js
@@ -2,14 +2,30 @@ define(['jquery', 'bootstrap'], function () {
     var main = {
         init: function () {
             $('[data-toggle="tooltip"]').tooltip(
-                { "trigger": "hover" }
+                {"trigger": "hover"}
             );
             $('#js_disclose_password').click(function () {
-                if($(this).prop('checked')){
-                    $('#register_form_password').attr('type','text')
+                if ($(this).prop('checked')) {
+                    $('#js_password_input').attr('type', 'text')
                 } else {
-                    $('#register_form_password').attr('type','password')
+                    $('#js_password_input').attr('type', 'password')
                 }
+            });
+            // imitate clicking on file button
+            $('#js_file_input_trigger').on('click touchstart', function (event) {
+                event.preventDefault();
+                $('#js_file_input').click();
+            });
+            // write filename after file select
+            $('#js_file_input').on('change', function(){
+                var filename = $(this).val().split('\\').pop();
+                $('#js_filename_area').val(filename);
+            });
+            // need to imitate selector
+            $('#js_gender_select').on('click touchstart', 'button',function(event){
+                event.preventDefault();
+                $('#js_gender_select').find('button').removeClass('active');
+                $(this).addClass('active');
             })
         }
 
